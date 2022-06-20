313594af176f136ba327308451303a03060b4fd0
Stephen <steve.wiggins7@gmail.com>
2022-06-19 23:23:13 -0400

    ping command works

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-const {Client, Intents} = require('discord.js');
+const {Client, Intents, Collection} = require('discord.js');
+const fs = require('fs');
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 require('dotenv').config();
 
-client.on('ready', () =>{
-    console.log("Ready");
-});
+client.commands = new Collection();
+
+const functions =  fs.readdirSync("./src/functions").filter(file => file.endsWith("js"));
+const commandFolders =  fs.readdirSync("./src/commands");
+const eventFiles =  fs.readdirSync("./src/events").filter(file => file.endsWith("js"));
+
 
 (async () => {
+    for(file of functions){
+        require(`./functions/${file}`)(client);
+    }
+    client.handleEvents(eventFiles, "./src/events");
+    client.handleCommands(commandFolders, "./src/commands");
     client.login(process.env.token);
 })();
